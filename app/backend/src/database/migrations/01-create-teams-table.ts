86ae37dab808373c6928ec9472651d2488a2a451
Shanloo <silmarfnascimento@gmail.com>
2023-08-25 16:40:55 -0300

    WIP req01: implementa a interface ITeam, a migration e a Model da entidade Team

---
+++ app/backend/src/database/migrations/01-create-teams-table.ts
@@ -0,0 +1,22 @@
+import { DataTypes, Model, QueryInterface } from "sequelize";
+import ITeam from '../../Interfaces/teams/ITeams';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      teamName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    })
+  },
+  down(queryInterface: QueryInterface) {
+
+  },
+}
---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,31 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
